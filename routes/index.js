79056f2db6a49459b7d5ae8e61ef3c711fa7ad88
Gabryel <gabryelsoh@gmail.com>
2020-09-22 18:24:10 +0800

    index get route missing let book for block scope

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const Book = require('../models/book');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get('/', async (req, res) => {
+    // declare books as a variable in the parent function first so that we can reference it later in the res.render
+    let books
+    try {
+        books = await Book.find().sort({ createdAt: 'desc'}).limit(10).exec()
+    } catch {
+        books = []
+    }
+    res.render('index', {
+        books: books
+    });
 });
 
 module.exports = router;
